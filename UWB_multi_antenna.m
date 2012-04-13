bbcbb607182dd066d7bad2a8f782946d0941acfe
Tobias <X.Volkmar@gmail.com>
2012-04-13 09:20:21 +0200

    first multi antenna

--- UWB_multi_antenna.m
+++ UWB_multi_antenna.m
@@ -5,22 +5,21 @@
 
 function X= UWB_single_antenna()
 	%% Eingabewerte 4x1
-	X=[1:4]';
-
-	%% Ausgabewerte 4x1
-	Y=zeros(4,1);
+	X=[1:4; 5:8; 9:12; 13:16]
 
 	%% Übertragungsfunktion
 	h=rand(4,4)+i*rand(4,4);
 
 	%%IFFT, Faltung und FFT
-	x=ifft(X);
-	y=cconv(h(:,1),x);
-	Y=fft(y);
-	H=fft(h);
-
+	for i=1:4
+		x=ifft(X(:,i));
+		y=cconv(h(:,i),x);
+		Y(:,i)=fft(y);
+		H(:,i)=fft(h(:,i));
+	end
+	
 	%%Decoder
-	X=Y./H(:,1);
+	X=Y./H;
 endfunction
 
 function Y=cconv(x,y)
